c7aca3a241ba1eedf602d5f6d13a4610cd31bbbc
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2022-11-23 13:08:28 +0100

    add starting and ending game functionality

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+var score = "0";
+var highScore = "0";
+const playButton =  document.getElementById('start');
+
 function showMenu(menu)
 {
     menu.classList.add('open');
-    menu.backgroundColor = "red";
 }
 function hideMenu(menu)
 {
     menu.classList.remove('open');
 }
 
+function setTimer()//do 3 sec countdown
+{
+
+} 
+function startGame()
+{   
+    hideMenu(main);
+    showMenu(stats);
+    setTimer();
+    setInterval(()=>{hideMenu(stats); showMenu(gameOver)}, 2000 );
+}
+
 
 showMenu(main);
+
+playButton.addEventListener('click', () => {
+    startGame();
+})
+
+
+
+
 
 
 
