b4af6d715601c2ee5242e985b923c8ba4856fee2
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2022-11-23 12:46:56 +0100

    add option to show/hide menus

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMenu(menu)
+{
+    menu.classList.add('open');
+    menu.backgroundColor = "red";
+}
+function hideMenu(menu)
+{
+    menu.classList.remove('open');
+}
+
+
+showMenu(main);
+
+
+
